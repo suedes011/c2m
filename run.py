346967b19d72cc81729587c2065951dba1fa4c14
suedes011 <102919603+suedes011@users.noreply.github.com>
2022-10-28 08:44:36 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ print("MENU")
 print("------------------------------------------")
 print("1. knn")
 print("2. pca")
+print("3. decision tree")
 
 ch = int(input("Enter your choice : "))
 file = ""
@@ -11,6 +12,8 @@ if ch == 1:
     file = "knn"
 elif ch == 2:
     file = "pca"
+elif ch == 3:
+    file = "tic_tac_toe_decisiontree"
 else:
     print("wrong option")
     
